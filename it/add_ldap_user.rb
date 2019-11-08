0197beed8b9acf371c55a802e9e7e8415dcb4670
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2019-11-08 15:43:52 +0100

    wording, capitalize step descriptions

--- it/add_ldap_user.rb
+++ it/add_ldap_user.rb
@@ -7,18 +7,18 @@ require './lib/it/add_user_ldif'
 
 runbook = Runbook.book "IT/Add LDAP User" do
   description <<~DESC
-    This is a runbook that describes how to set up a user in our LDAP
+    This is a runbook that describes how to set up a new user in our LDAP
   DESC
 
   section "Generate LDIF file" do
-    step "gather info" do
+    step "Gather new user information" do
       ask 'E-Mail-Address of user:', into: 'email'
       ask 'First Name of user:',     into: 'first_name'
       ask 'Last Name of user:',      into: 'last_name'
       ask 'Last UID/GID in LDAP:',    into: 'last_gid'
     end
 
-    step "generate password" do
+    step "Generate password" do
       path "/home/felix/projects/self/glueckskeks/"
       capture %Q{bundle exec exe/glueckskeks}, into: :passphrase
       ruby_command do
@@ -26,7 +26,7 @@ runbook = Runbook.book "IT/Add LDAP User" do
       end
     end
 
-    step "output ldif file" do
+    step "Generate ldif data" do
       ruby_command do
         @user = Operations::IT::LDAP::User.new first_name: @first_name,
           last_name: @last_name,
@@ -38,7 +38,7 @@ runbook = Runbook.book "IT/Add LDAP User" do
       end
     end
 
-    step "save as ldif file" do
+    step "Save as ldif file" do
       ruby_command do
         filename = "/tmp/#{@user.uid}.ldif"
         File.write(filename, @ldif)
@@ -46,10 +46,16 @@ runbook = Runbook.book "IT/Add LDAP User" do
       end
     end
 
-    step "import ldif file in ldap" do
+    step "Import ldif file in LDAP" do
     end
 
-    step "print or write down password (with phrase) for user" do
+    step "Reflect on Group memberships" do
+    end
+
+    step "Print or write down password (with phrase) for new user" do
+    end
+
+    step "Deliver information to new user" do
     end
   end
 end
